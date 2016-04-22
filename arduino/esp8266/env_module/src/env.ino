a25dbea94dc97387062caf8b70996e59d79245a8
Jacob Sohl <jacobsohl@gmail.com>
2016-04-22 15:11:08 -0700

    env module

---
+++ arduino/esp8266/env_module/src/env.ino
@@ -0,0 +1,15 @@
+#include "DHT.h"
+#include "ESPHttpServer.h"
+int dht_pin=1;
+int light_pin=2;
+
+DHT=dht22
+void setup(){
+  pinMode($dht_pin,INPUT);
+  pinMode($light_pin,INPUT);
+
+}
+
+void loop(/* arguments */) {
+  /* code */
+}
