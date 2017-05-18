78622f1a776a5ed938b8da42f5104dff318cc374
Jacob Sohl <jacobsohl@gmail.com>
2017-05-17 17:00:18 -0700

    clean up

--- iostat.py
+++
@@ -1,7 +0,0 @@
-def log(level,msg):
-    if level == "error":
-        print "ERROR " + msg
-    if level == "debug":
-        print "DEBUG " + msg
-    if level == "info":
-        print "INFO " + msg
