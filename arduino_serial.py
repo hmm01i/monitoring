20d1172035095b096e70721ae6788616599f807d
Jacob Sohl <jacobsohl@gmail.com>
2017-05-17 17:15:14 -0700

    iterative changes and practice
    this should probably go in scratch repo

--- arduino_serial.py
+++ arduino_serial.py
@@ -2,4 +2,10 @@ import serial
 
 uno = serial.Serial('/dev/ttyACM0',9600)
 while True:
-    print uno.readline()
+    try:
+        val = int(uno.readline())
+        if val != 0:
+            print val
+    except ValueError:
+            print "we got a strange value"
+
